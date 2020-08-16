39532604245eda02c85fa57b420fc139fa939877
Foysal Ahammed <foysal.developer@gmail.com>
2020-08-17 03:49:51 +0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const serachBtn = document.getElementById('search-button');
+const songLyrics = document.getElementById('lyrics');
+const searchField = document.getElementById('search-input');
+const serachResult = document.getElementById('search-result');
+
+//Clicking on the search  button
+serachBtn.addEventListener('click', handleSearch);
+
+//handeling the search
+function handleSearch() {
+  const searchValue = searchField.value;
+  const url = `https://api.lyrics.ovh/suggest/${searchValue}`;
+  //removing previous song lyrics
+  songLyrics.innerHTML = '';
+  //Fetching data from the api
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => displaySearchResult(jsonData.data));
+  //Diplaying data to the User Interface
+}
+//Displaying serach result to the UI
+function displaySearchResult(songs) {
+  console.log(songs);
+  serachResult.innerHTML = '';
+  if (songs.length === 0) {
+    serachResult.innerHTML = 'No song found. Try with another keyword';
+  }
+  const songData = songs.slice(0, 10);
+  songData.map((song) => {
+    serachResult.innerHTML += `
+                <div class="single-result row align-items-center my-3 p-3">
+                <div class="col-md-9">
+                  <h3 class="lyrics-name">${song.title}</h3>
+                  <p class="author lead">Album  ${song.album.title} <br> Album by <span>${song.artist.name}</span></p>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                  <button class="btn btn-success" onclick = "handleLyrics('${song.artist.name}','${song.title}')">Get Lyrics</button>
+                </div>
+              </div>`;
+  });
+}
+//Fetching song lyrics
+function handleLyrics(title, artist) {
+  const url = `https://api.lyrics.ovh/v1/${title}/${artist}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => displayLyrics(jsonData, title));
+}
+//Displaying song lyrics to the UI
+function displayLyrics(data, title) {
+  if (!data.lyrics) {
+    songLyrics.innerHTML = `
+        <button class="btn go-back">&lsaquo;</button>
+          <h2 class="text-success mb-4">${title}</h2>
+          <pre class="lyric text-white">${data.error}</pre>`;
+  } else {
+    songLyrics.innerHTML = `
+        <button class="btn go-back">&lsaquo;</button>
+          <h2 class="text-success mb-4">${title}</h2>
+          <pre class="lyric text-white">${data.lyrics}</pre>`;
+  }
+}
